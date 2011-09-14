373c8dd3ec1980724531c3931e16b55fc11e5517
Zachary Anker <zach.anker@gmail.com>
2011-09-13 18:53:49 -0700

    Added sorted classes for the nav bar Added an index page Fix route file to use the actual Rails one not a debug Fixed the end file path, since we write a file per method, it's controller/method.html not just controller.html Fixed copying of images and javascript from the template to the doc Fixed calling render_file in an ERB template causing the buffer to reset

--- lib/rdoc/generator/rest.rb
+++ lib/rdoc/generator/rest.rb
@@ -29,15 +29,17 @@ class RDoc::Generator::REST
 
   def generate(top_levels)
     @output_dir = Pathname.new(@options.op_dir).expand_path(@base_dir)
+    @current_file, @current_method = nil, nil
 
     # Load external routing map in place of in-comment attributes
-    route_file = File.join(@base_dir, "test.txt")
+    route_file = File.join(@base_dir, "/tmp/routes.txt")
     @routes = YAML.load_file(route_file) if File.exists?(route_file)
 
-    copy_assets
+    @sorted_classes = []
 
     # Run through every file and parse out any metadata that we use.
     # Can't use the preprocessor data as that's saved per class, and not per method like we need.
+    # Figure out what classes and methods we can use beforehand for when we generate the index and the files
     top_levels.each do |file|
       file.classes.each do |klass|
         mkpath = nil
@@ -46,27 +48,44 @@ class RDoc::Generator::REST
         # Classes are public by default, but can be forced private if needed
         next if klass.metadata["api_status"] == "private"
 
+        methods = []
+
         klass.each_method do |method|
           parse_metadata(method.comment, method, "method_name|api_status|path|http_req")
           # Methods by default are private unless otherwise
           next if method.metadata["api_status"] != "public" && method.visibility == :public
 
           unless mkpath
-            Pathname.new(@output_dir + file.path).dirname.mkpath
+            Pathname.new("#{@output_dir}#{method_path(file, method)}").dirname.mkpath
             mkpath = true
           end
 
-          write_method(file, klass, method)
+          methods.push(method)
         end
+
+        @sorted_classes.push([file, klass, methods]) unless methods.empty?
+      end
+    end
+
+    @sorted_classes = @sorted_classes.sort {|a, b| a[0] <=> b[0]}
+
+    copy_assets
+    write_index
+
+    @sorted_classes.each do |file, klass, methods|
+      @current_class = klass
+      methods.each do |method|
+        @current_method = method
+        write_method(file, klass, method)
       end
     end
   end
 
   def copy_assets
     options = {:verbose => $DEBUG_RDOC, :noop => @options.dry_run}
-    FileUtils.cp(@template_dir + "rdoc.css", ".", options)
+    FileUtils.cp("#{@template_dir}/rdoc.css", ".", options)
 
-    Dir["#{@template_dir}{js,images}/**/*"].each do |path|
+    Dir["#{@template_dir}/{js,images}/**/*"].each do |path|
       next if File.directory?(path) or File.basename(path) =~ /^\,/
 
       dest = Pathname.new(path).relative_path_from(@template_dir)
@@ -77,14 +96,30 @@ class RDoc::Generator::REST
     end
   end
 
+  def write_index
+    title = @options.main_page || "API Index"
+    output_file = Pathname.new("#{@output_dir}/index.html")
+    asset_prefix = @output_dir.relative_path_from(output_file.dirname)
+
+    @context = binding
+
+    write_file(output_file, "index.rhtml")
+  end
+
+  def method_path(file, method)
+    "/#{file.relative_name.gsub(/\.([a-zA-Z]+)$/, "")}/#{method.name}.html"
+  end
+
   def write_method(file, klass, method)
     class_name = klass.metadata["class_name"] || klass.full_name
-    method_name = method.metadata["method_name"] || method.name
+    method_name = method.metadata["method_name"] || method.name.gsub("_", " ").capitalize
     title = "#{class_name} -> #{method_name}"
+    title << " | #{@options.title}" unless @options.title.nil?
 
-    output_file = Pathname.new(@output_dir + klass.path)
+    output_file = Pathname.new("#{@output_dir}#{method_path(file, method)}")
     asset_prefix = @output_dir.relative_path_from(output_file.dirname)
     api_route, @context = @routes && @routes["#{file.relative_name}/#{method.name}"], binding
+    current_file, current_method = file, method
 
     # Default to whatever is set in the API
     unless api_route
@@ -96,6 +131,10 @@ class RDoc::Generator::REST
       raise RDoc::Error.new("error generating #{output_file}: Missing either http_req or path to the API")
     end
 
+    write_file(output_file, "page.rhtml")
+  end
+
+  def write_file(output_file, to_render)
     content = <<-HTML
 <!DOCTYPE html>
 <html>
@@ -103,7 +142,7 @@ class RDoc::Generator::REST
     #{render_file("_header.rhtml")}
   </head>
 
-  #{render_file("page.rhtml")}
+  #{render_file(to_render, "_erbfile")}
 
   #{render_file("_footer.rhtml")}
 </html>
@@ -117,12 +156,12 @@ class RDoc::Generator::REST
     end
   end
 
-  def render_file(path)
+  def render_file(path, eout="_erbout")
     if source = @template_cache[path]
       return source.result(@context)
     end
 
-    @template_cache[path] = ERB.new(File.read(@template_dir + path), nil, "<>")
+    @template_cache[path] = ERB.new(File.read(@template_dir + path), nil, "<>", eout)
     @template_cache[path].result(@context)
   end
 
