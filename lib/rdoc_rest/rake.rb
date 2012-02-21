8ae357ae80e840d4609194c5b18947e1bd52f58c
Zachary Anker <zach.anker@gmail.com>
2012-02-21 09:11:51 -0800

    Updated for Rails 3.2

--- lib/rdoc_rest/rake.rb
+++ lib/rdoc_rest/rake.rb
@@ -6,11 +6,11 @@ namespace :rest do
     Rails.application.routes.routes.each do |route|
       next unless route.app.class.name.to_s =~ /^ActionDispatch::Routing/
       reqs = route.requirements
-      path = route.path
+      path = route.path.spec.to_s
       path = path.gsub("(.:format)", "") if ENV["no_format"]
 
-      data["app/controllers/#{reqs[:controller]}_controller.rb/#{reqs[:action]}"] = {:path => path, :type => route.verb.to_s}
-    end
+      data["app/controllers/#{reqs[:controller]}_controller.rb/#{reqs[:action]}"] = {:path => path, :type => route.verb.source.gsub(/[$^]/, "")}
+     end
 
     file = File.open(File.join(Rails.root, "tmp", "routes.txt"), "w+")
     file.write(data.to_yaml)
